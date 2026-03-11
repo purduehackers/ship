8a22d2c93543e43164dbe6f86d758903c9396e62
Jack Hogan <jackhogan11@gmail.com>
2026-03-11 11:00:41 -0400

    favicon, db config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import './layout.css';
 	import Nav from '$lib/components/Nav.svelte';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
+	import faviconLight from '$lib/assets/favicon-light.png';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} media="(prefers-color-scheme: light)" />
+	<link rel="icon" href={faviconLight} media="(prefers-color-scheme: dark)" />
+</svelte:head>
 <Nav />
 <div class="pt-8">
 	{@render children()}
